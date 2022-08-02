39c6bc5715f53a4edafa052907a16aed38e51368
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-08-02 11:22:33 +0800

    address issue

--- src/port-sniffer.rs
+++ src/port-sniffer.rs
@@ -1,5 +1,5 @@
-use std::net::{IpAddr, TcpStream};
-use std::str::FromStr;
+use core::time;
+use std::net::{TcpStream, ToSocketAddrs};
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 use std::{
@@ -18,12 +18,24 @@ struct Arguments {
 
     #[structopt(short, long, default_value = "2000")]
     threads: u16,
+
+    #[structopt(short = "o", long, default_value = "5")]
+    timeout: u64,
 }
 
-fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+fn scan(
+    tx: Sender<u16>,
+    start_port: u16,
+    host: String,
+    num_threads: u16,
+    timeout: u64,
+) -> Result<(), Box<dyn error::Error>> {
     let mut port: u16 = start_port + 1;
+    let timeout = time::Duration::from_secs(timeout);
     loop {
-        if TcpStream::connect((addr, port)).is_ok() {
+        let addr = format!("{}:{}", host, port);
+        let addr = addr.to_socket_addrs()?.next().unwrap();
+        if TcpStream::connect_timeout(&addr, timeout).is_ok() {
             print!(".");
             io::stdout().flush().unwrap();
             tx.send(port).unwrap();
@@ -34,19 +46,20 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
         }
         port += num_threads;
     }
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let arguments = Arguments::from_args();
-    let addr = IpAddr::from_str(&arguments.ipaddr)?;
 
     let num_threads = arguments.threads;
     let (tx, rx) = channel();
     for i in 0..num_threads {
         let tx = tx.clone();
+        let host = arguments.ipaddr.clone();
 
         thread::spawn(move || {
-            scan(tx, i, addr, num_threads);
+            let _ = scan(tx, i, host, num_threads, arguments.timeout);
         });
     }
 
