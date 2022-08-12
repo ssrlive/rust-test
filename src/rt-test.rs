b49297cb6b2e88c10fd818aa83d79e477877629d
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-08-12 12:58:59 +0800

    anyhow library usage

--- src/rt-test.rs
+++ src/rt-test.rs
@@ -2,17 +2,37 @@ use chrono::Local;
 use std::thread;
 use tokio::{self, runtime::Runtime, task, time};
 
-fn now() -> String {
+fn _now() -> String {
     Local::now().format("%F %T").to_string()
 }
 
-fn main() {
+fn _main() {
     let rt = Runtime::new().unwrap();
     let _guard = rt.enter();
     let _h = task::spawn(async {
         time::sleep(time::Duration::from_secs(3)).await;
-        println!("task over: {}", now());
+        println!("task over: {}", _now());
     });
-    println!("task over: {}", now());
+    println!("task over: {}", _now());
     thread::sleep(time::Duration::from_secs(4));
+}
+
+use anyhow::Result;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let n = contents.trim().parse::<i32>()?;
+    Ok(2 * n)
+}
+
+fn main() {
+    match file_double("foobar") {
+        Ok(n) => println!("{}", n),
+        Err(err) => println!("Error: {:?}", err),
+    }
 }
