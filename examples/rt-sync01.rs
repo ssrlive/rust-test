86e34cb7abb29aa5b736fc9924bef3e081e2f696
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-11-03 12:19:24 +0800

    play toys

--- examples/rt-sync01.rs
+++ examples/rt-sync01.rs
@@ -19,4 +19,26 @@ fn main() {
         };
         // tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     });
+
+    rt.block_on(async {
+        let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(1));
+        let (tx, mut rx) = sync::oneshot::channel::<i32>();
+        tokio::spawn(async move {
+            tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+            if let Err(e) = tx.send(33) {
+                println!("some error \"{}\"", e);
+            }
+        });
+        loop {
+            tokio::select! {
+                _ = interval.tick() => {
+                    println!("tick");
+                }
+                msg = &mut rx => {
+                    println!("got message: {:?}", msg.unwrap());
+                    break;
+                }
+            }
+        }
+    });
 }
